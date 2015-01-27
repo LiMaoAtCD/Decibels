0839936707ce110823ca0e68527a68e2d7c57695
liamscofield <liamscofield@gmail.com>
2015-01-27 18:03:49 +0800

    decibels demo

--- Decibels/ViewController.swift
+++ Decibels/ViewController.swift
@@ -7,12 +7,53 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
-
+    var recorder: AVAudioRecorder?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
+        
+        var url = NSURL.fileURLWithPath("/dev/null")
+        
+        var settings: [NSString: NSNumber] = [ AVSampleRateKey: 44100,
+            AVFormatIDKey: kAudioFormatAppleLossless,
+            AVNumberOfChannelsKey: 2,
+            AVEncoderAudioQualityKey: AVAudioQuality.Max.rawValue
+        ]
+        var error: NSError?
+        
+        recorder = AVAudioRecorder(URL: url!, settings: settings, error: &error)
+        
+        if recorder != nil {
+            recorder!.prepareToRecord()
+            recorder!.meteringEnabled = true
+            recorder!.record()
+            var levelTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "levelTimerCallback:", userInfo: nil, repeats: true)
+        } else {
+            println(error!.description)
+        }
+        
+//        var avsession = AVCaptureSession()
+//        var captureInputPort = AVCaptureInputPort()
+//    
+//        var captureConnection: AVCaptureConnection = AVCaptureConnection(inputPort: <#AVCaptureInputPort!#>, videoPreviewLayer: <#AVCaptureVideoPreviewLayer!#>))
+    }
+    
+    func levelTimerCallback(timer:NSTimer) -> Void {
+        recorder?.updateMeters()
+        
+        var level: Float
+        
+        var minDecibels: Float = -80.0
+        var decibels = recorder?.averagePowerForChannel(0)
+        
+        println("Decibels:\(decibels!)")
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
